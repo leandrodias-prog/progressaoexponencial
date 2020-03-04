c390050ca286600618d9bad9a4bde76cd24c73ba
Leandro Santos Dias <60950142+leandrodias-prog@users.noreply.github.com>
2020-03-03 22:05:03 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,10 @@
+#autores:@leandrodias
+#feito em :03/03/2020
+#nstituição: Etec
+
+x=0  #Declaração do contado"
+valor=2  #Declaração da variavel
+while (x < 21) :
+        valor = valor * valor  
+        X= X + 1#"Laço de repetição"
+print (valor) #Exibir variavel do valor"
